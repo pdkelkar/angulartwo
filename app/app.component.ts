9c10e2fb424722a025650d34874eae506c760c43
Priyadarshan Kelkar <priyadarshan.kelkar@optus.com.au>
2016-09-08 15:24:51 +1000

    mosh second exercise

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,15 +3,31 @@ import { Component } from '@angular/core';
 import { CourseComponent } from './course/index';
 import { AuthorComponent } from './author/index';
 import { StarComponent } from './star/index';
+import { HeartComponent } from './heart/index';
+import { VoterComponent } from './voter/index';
 
 @Component({
   selector: 'my-app',
   template: `
   <h1>My First Angular 2 App</h1>
-  <course></course>
+  <hr>
+  <!-- <course></course>
   <author></author>
-  <twinkle></twinkle>
+  <twinkle [is-favourite]="post.favouriteValue"  (starHasChanged)="onStarChange($event)"></twinkle>
+  <crush></crush> -->
+  <voter></voter>
   `
   
 })
-export class AppComponent { }
+export class AppComponent { 
+
+  post = {
+    title: 'Some Title',
+    favouriteValue: true
+  }
+
+  onStarChange($event){
+    console.log($event);
+  }
+
+}
--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { CourseComponent } from './course/index';
 import { AuthorComponent } from './author/index';
 import { StarComponent } from './star/index';
+import { HeartComponent } from './heart/index';
+import { VoterComponent } from './voter/index';
 import { AutoGrowDirective } from './auto-grow.directive';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [AppComponent, CourseComponent, AuthorComponent, StarComponent, AutoGrowDirective],
+  declarations: [AppComponent, CourseComponent, AuthorComponent, StarComponent, HeartComponent, VoterComponent, AutoGrowDirective],
   bootstrap: [AppComponent]  
 })
 export class AppModule { }
